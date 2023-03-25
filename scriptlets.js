37c725393222c2b3ca5b1557a818d06086027472
uBlock-user <sscarl24@gmail.com>
2023-03-26 01:18:11 +0530

    Update add-class and set-attr

--- scriptlets.js
+++ scriptlets.js
@@ -69,62 +69,63 @@ function(
 /// set-attr.js
 /// alias sa.js
 // example.com##+js(sa, attr, value, [selector])
-(() => {
-		  const token = '{{1}}';
-		  if ( token === '' || token === '{{1}}' ) { return; }
-		  const tokens = token.split(/\s*\|\s*/);
-		  const attrValue = '{{2}}';
-		  let selector = '{{3}}';
-		  if ( selector === '' || selector === '{{3}}' ) { selector = `[${tokens.join('],[')}]`; }
-		  const behavior = '{{4}}';
-		  let timer = undefined;
-		  const setattr = () => {  
-			const nodes = document.querySelectorAll(selector);
-			try {
-				for (const node of nodes) {
-					for ( const attr of tokens ) {
-					      if ( attr !== attrValue) { 
-					      	   node.setAttribute(attr, attrValue);
-					      }	      
-					}
-				}
-			} catch { }
-		  };
-		  const mutationHandler = mutations => {
-			if ( timer !== undefined ) { return; }
-			let skip = true;
-			for ( let i = 0; i < mutations.length && skip; i++ ) {
-			    const { type, addedNodes, removedNodes } = mutations[i];
-			    if ( type === 'attributes' ) { skip = false; }
-			    for ( let j = 0; j < addedNodes.length && skip; j++ ) {
-				if ( addedNodes[j].nodeType === 1 ) { skip = false; break; }
-			    }
-			    for ( let j = 0; j < removedNodes.length && skip; j++ ) {
-				if ( removedNodes[j].nodeType === 1 ) { skip = false; break; }
-			    }
+function(
+	token = '',
+	attrValue = '',
+	selector = '',
+	behavior = '' 
+) {
+	if ( token === '' ) { return; }
+	const tokens = token.split(/\s*\|\s*/);
+	if ( selector === '' ) { selector = `[${tokens.join('],[')}]`; }
+	let timer = undefined;
+	const setattr = () => {  
+	const nodes = document.querySelectorAll(selector);
+	try {
+		for (const node of nodes) {
+			for ( const attr of tokens ) {
+			      if ( attr !== attrValue) { 
+				   node.setAttribute(attr, attrValue);
+			      }	      
 			}
-			if ( skip ) { return; }
-			timer = self.requestIdleCallback(setattr, { timeout: 10 });
-		  };
-		  const start = ( ) => {
-			setattr();
-			if ( /\bloop\b/.test(behavior) === false ) { return; }
-			const observer = new MutationObserver(mutationHandler);
-			observer.observe(document.documentElement, {
-			    attributes: true,
-			    attributeFilter: tokens,
-			    childList: true,
-			    subtree: true,
-			});
-		  };
-		  if ( document.readyState !== 'complete' && /\bcomplete\b/.test(behavior) ) {
-			self.addEventListener('load', start, { once: true });
-		     } else if ( document.readyState === 'loading' ) {
-			self.addEventListener('DOMContentLoaded', start, { once: true });
-		     } else {
-			start();
-		  }
-})();
+		}
+	} catch { }
+	};
+	const mutationHandler = mutations => {
+	if ( timer !== undefined ) { return; }
+	let skip = true;
+	for ( let i = 0; i < mutations.length && skip; i++ ) {
+	    const { type, addedNodes, removedNodes } = mutations[i];
+	    if ( type === 'attributes' ) { skip = false; }
+	    for ( let j = 0; j < addedNodes.length && skip; j++ ) {
+		if ( addedNodes[j].nodeType === 1 ) { skip = false; break; }
+	    }
+	    for ( let j = 0; j < removedNodes.length && skip; j++ ) {
+		if ( removedNodes[j].nodeType === 1 ) { skip = false; break; }
+	    }
+	}
+	if ( skip ) { return; }
+	timer = self.requestIdleCallback(setattr, { timeout: 10 });
+	};
+	const start = ( ) => {
+	setattr();
+	if ( /\bloop\b/.test(behavior) === false ) { return; }
+	const observer = new MutationObserver(mutationHandler);
+	observer.observe(document.documentElement, {
+	    attributes: true,
+	    attributeFilter: tokens,
+	    childList: true,
+	    subtree: true,
+	});
+	};
+	if ( document.readyState !== 'complete' && /\bcomplete\b/.test(behavior) ) {
+	self.addEventListener('load', start, { once: true });
+	} else if ( document.readyState === 'loading' ) {
+	self.addEventListener('DOMContentLoaded', start, { once: true });
+	} else {
+	start();
+	}
+}
 
 /// rename-attr.js
 /// alias rna.js
@@ -187,27 +188,28 @@ function(
 /// add-class.js
 /// alias ac.js
 // example.com##+js(ac, class, [selector])
-(() => {
-		    const needle = '{{1}}';
-		    if ( needle === '' || needle === '{{1}}' ) { return; }
-		    const needles = needle.split(/\s*\|\s*/);
-		    let selector = '{{2}}';
-		    if ( selector === '' || selector === '{{2}}' ) { selector = '.' + needles.map(a => CSS.escape(a)).join(',.'); }
-		    const addclass = ev => {
-						if (ev) { self.removeEventListener(ev.type, addclass, true);  }
-						const nodes = document.querySelectorAll(selector);
-						try {
-							for ( const node of nodes ) {
-							      node.classList.add(...needles);
-							}
-						} catch { }
-		    };
-		    if (document.readyState === 'loading') {
-		    	      self.addEventListener('DOMContentLoaded', addclass, true);
-	   	    } else {
-		    	      addclass();
-	   	    }
-})();
+function(
+	needle = '',
+	selector = '' 
+) {
+	if ( needle === '' ) { return; }
+	const needles = needle.split(/\s*\|\s*/);
+	if ( selector === '' ) { selector = '.' + needles.map(a => CSS.escape(a)).join(',.'); }
+	const addclass = ev => {
+		if (ev) { self.removeEventListener(ev.type, addclass, true);  }
+		const nodes = document.querySelectorAll(selector);
+		try {
+			for ( const node of nodes ) {
+			      node.classList.add(...needles);
+			}
+		} catch { }
+	};
+	if (document.readyState === 'loading') {
+	      self.addEventListener('DOMContentLoaded', addclass, true);
+	} else {
+	      addclass();
+	}
+}
 
 /// replace-class.js
 /// alias rpc.js
