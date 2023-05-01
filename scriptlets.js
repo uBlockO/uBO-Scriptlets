06fb5c243787ea621eb7284870e9c88a2328b4d0
uBlock-user <sscarl24@gmail.com>
2023-05-01 20:39:51 +0530

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -48,7 +48,7 @@ function removeNode(
 	} else {
 		  needle = needle.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 	}
-	needle = new RegExp(needle, "gms");
+	needle = new RegExp(needle, "g");
 	const removenode = () => {
 	        const nodes = document.getElementsByTagName(tagname);  
 		try {
