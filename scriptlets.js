8bc3dc549bc37a533a33cdd767211ee25e694bff
uBlock-user <21290713+uBlock-user@users.noreply.github.com>
2023-07-04 17:42:44 +0530

    Added in uBO
    https://github.com/gorhill/uBlock/commit/083a318090e38fb81c2b94ea326521d5a76f7c57

--- scriptlets.js
+++ scriptlets.js
@@ -111,7 +111,7 @@ function renameAttr(
 	let timer;
 	const renameattr = ( ) => {
 		timer = undefined;
-		const elems = document.querySelectorAll( selector );
+		const elems = document.querySelectorAll(selector);
 		try {
 			for ( const elem of elems ) {
 				if ( elem.hasAttribute( oldattr ) ) {
@@ -354,117 +354,6 @@ function noAlertIf(
                             }  
                         }
                 });
-}
-
-/// removeSessionItem.js
-/// alias rsi.js
-/// world ISOLATED
-// example.com##+js(rsi, key)
-function removeSessionItem( 
-        key = '',
-	runAt = '' 
-) { 
-	    if ( key === '' ) { return; }
-	    const keys = key.split(/\s*\|\s*/);
-	    let timer;
-	    const removeItem = () => {
-		  timer = undefined;  
-		  if ( key === '*' ) { return sessionStorage.clear(); }  
-		  try {
-			   for (const keyName of keys) {
-				sessionStorage.removeItem(keyName);
-			   }
-		  } catch { }
-	    };
-	    const mutationHandler = mutations => {
-		if ( timer !== undefined ) { return; }
-		let skip = true;
-		for ( let i = 0; i < mutations.length && skip; i++ ) {
-		    const { type, addedNodes, removedNodes } = mutations[i];
-		    if ( type === 'attributes' ) { skip = false; }
-		    for ( let j = 0; j < addedNodes.length && skip; j++ ) {
-			if ( addedNodes[j].nodeType === 1 ) { skip = false; break; }
-		    }
-		    for ( let j = 0; j < removedNodes.length && skip; j++ ) {
-			if ( removedNodes[j].nodeType === 1 ) { skip = false; break; }
-		    }
-		}
-		if ( skip ) { return; }
-		timer = self.requestIdleCallback(removeItem, { timeout: 10 });
-	    };
-	    const start = ( ) => {
-		removeItem();
-		if ( /\bloop\b/.test(runAt) === false ) { return; }
-		const observer = new MutationObserver(mutationHandler);
-		observer.observe(document.documentElement, {
-		    attributes: true,
-		    childList: true,
-		    subtree: true,
-		});
-	    };
-	    if ( document.readyState !== 'complete' && /\bcomplete\b/.test(runAt) ) {
-            self.addEventListener('load', start, { once: true });
-    	    } else if ( document.readyState !== 'loading' || /\basap\b/.test(runAt) ) {
-            start();
-    	    } else {
-            self.addEventListener('DOMContentLoaded', start, { once: true });
-    	    }
-}
-
-/// setSessionItem.js
-/// alias ssi.js
-/// world ISOLATED
-// example.com##+js(ssi, key, value)
-function setSessionItem(
-	key = '',
-	value = '',
-	runAt = ''
-) { 
-	    if ( key === '' ) { return; }
-	    const keys = key.split(/\s*\|\s*/);
-	    let timer;
-	    const setItem = () => {
-		  timer = undefined;  
-		  try {
-			   for (const keyName of keys) {
-				if (sessionStorage.getItem(keyName) === value) { break; }
-				    sessionStorage.setItem(keyName, value);
-			   }
-		  } catch { }
-	    };
-	    const mutationHandler = mutations => {
-		if ( timer !== undefined ) { return; }
-		let skip = true;
-		for ( let i = 0; i < mutations.length && skip; i++ ) {
-		    const { type, addedNodes, removedNodes } = mutations[i];
-		    if ( type === 'attributes' ) { skip = false; }
-		    for ( let j = 0; j < addedNodes.length && skip; j++ ) {
-			if ( addedNodes[j].nodeType === 1 ) { skip = false; break; }
-		    }
-		    for ( let j = 0; j < removedNodes.length && skip; j++ ) {
-			if ( removedNodes[j].nodeType === 1 ) { skip = false; break; }
-		    }
-		}
-		if ( skip ) { return; }
-		timer = self.requestIdleCallback(setItem, { timeout: 10 });
-	    };
-	    const start = ( ) => {
-		setItem();
-		if ( /\bloop\b/.test(runAt) === false ) { return; }
-		const observer = new MutationObserver(mutationHandler);
-		observer.observe(document.documentElement, {
-		    attributes: true,
-		    childList: true,
-		    subtree: true,
-		});
-	    };
-	    if ( document.readyState !== 'complete' && /\bcomplete\b/.test(runAt) ) {
-        self.addEventListener('load', start, { once: true });
-    	} else if ( document.readyState !== 'loading' || /\basap\b/.test(runAt) ) {
-        start();
-    	} else {
-        self.addEventListener('DOMContentLoaded', start, { once: true });
-    	}
 }
 
 /// insert-child-before.js
