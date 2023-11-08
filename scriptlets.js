45ac6e89b1c3710eb7d95586e5c0b551fd3dda6b
uBlock-user <21290713+uBlock-user@users.noreply.github.com>
2023-11-08 10:03:36 +0530

    Add set-attr scriptlet back

--- scriptlets.js
+++ scriptlets.js
@@ -487,3 +487,61 @@ function noBeaconIf(
         };
         runAt(( ) => { trapBeacons(); }, extraArgs.runAt);
 }
+
+/// trusted-set-attr.js
+/// alias tsa.js
+/// dependency run-at.fn
+/// world ISOLATED
+// example.com##+js(tsa, attr, value, [selector])
+function setAttr(
+	token = '',
+	attrValue = '',
+	selector = '',
+	run = '' 
+) {
+	if ( token === '' ) { return; }
+	const tokens = token.split(/\s*\|\s*/);
+	if ( selector === '' ) { selector = `[${tokens.join('],[')}]`; }
+	let timer;
+	const setattr = () => {
+	timer = undefined;	
+	const nodes = document.querySelectorAll(selector);
+	try {
+		for (const node of nodes) {
+			for ( const attr of tokens ) {
+			      if ( attr !== attrValue) { 
+				   node.setAttribute(attr, attrValue);
+			      }	      
+			}
+		}
+	} catch { }
+	};
+	const mutationHandler = mutations => {
+	if ( timer !== undefined ) { return; }
+	let skip = true;
+	for ( let i = 0; i < mutations.length && skip; i++ ) {
+	    const { type, addedNodes, removedNodes } = mutations[i];
+	    if ( type === 'attributes' ) { skip = false; }
+	    for ( let j = 0; j < addedNodes.length && skip; j++ ) {
+		if ( addedNodes[j].nodeType === 1 ) { skip = false; break; }
+	    }
+	    for ( let j = 0; j < removedNodes.length && skip; j++ ) {
+		if ( removedNodes[j].nodeType === 1 ) { skip = false; break; }
+	    }
+	}
+	if ( skip ) { return; }
+	timer = self.requestAnimationFrame(setattr);
+	};
+	const start = ( ) => {
+	setattr();
+	if ( /\bloop\b/.test(run) === false ) { return; }
+	const observer = new MutationObserver(mutationHandler);
+	observer.observe(document.documentElement, {
+	    attributes: true,
+	    attributeFilter: tokens,
+	    childList: true,
+	    subtree: true,
+	});
+	};
+	runAt(( ) => { start(); }, /\bcomplete\b/.test(run) ? 'idle' : 'interactive');
+}
