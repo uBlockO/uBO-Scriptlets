5a4a7f454d188a11c5ef675adf1614244a3d62b1
uBlock-user <21290713+uBlock-user@users.noreply.github.com>
2026-05-21 14:19:31 +0530

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -11,6 +11,13 @@ function removeDOMElement(
 ) {
     const safe = safeSelf();
     const logPrefix = safe.makeLogPrefix('removeDOMElement', ...Array.from(arguments));
+	const extraArgs = safe.getExtraArgs(Array.from(arguments), 3);
+	const reIncludes = extraArgs.includes || extraArgs.condition
+        ? safe.patternToRegex(extraArgs.includes || extraArgs.condition, 'ms')
+        : null;
+    const reExcludes = extraArgs.excludes
+        ? safe.patternToRegex(extraArgs.excludes, 'ms')
+        : null;
     const stop = (takeRecord = true) => {
         if ( takeRecord ) {
             handleMutations(observer.takeRecords());
@@ -20,9 +27,17 @@ function removeDOMElement(
             safe.uboLog(logPrefix, 'Quitting');
         }
     };
-    let sedCount = 0;
+    let sedCount = extraArgs.sedCount || 0;
     const handleNode = node => { 
 		const nodes = document.querySelectorAll(selector);
+		if ( reIncludes ) {
+            reIncludes.lastIndex = 0;
+            if ( safe.RegExp_test.call(reIncludes, before) === false ) { return true; }
+        }
+		if ( reExcludes ) {
+            reExcludes.lastIndex = 0;
+            if ( safe.RegExp_test.call(reExcludes, before) ) { return true; }
+        }
 		try {
 			  for ( const elem of nodes ) {
 				  if ( elem ) {
@@ -54,13 +69,15 @@ function removeDOMElement(
             const node = treeWalker.nextNode();
             count += 1;
             if ( node === null ) { break; }
+			if ( node === document.currentScript ) { continue; }
             if ( handleNode(node) ) { continue; }
             stop(); break;
         }
         safe.uboLog(logPrefix, `${count} nodes present before installing mutation observer`);
     }
+	if ( extraArgs.stay ) { return; }
     runAt(( ) => {
-        const quitAfter = 0;
+        const quitAfter = extraArgs.quitAfter || 0;
         if ( quitAfter !== 0 ) {
             setTimeout(( ) => { stop(); }, quitAfter);
         } else {
